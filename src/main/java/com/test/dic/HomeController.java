6c6bc9bc824c0d7d26a3fcc8d7aea6e029448068
kimdoyeon <qpt0707@daum.net>
2017-09-01 09:51:38 +0900

    note_book

--- src/main/java/com/test/dic/HomeController.java
+++ src/main/java/com/test/dic/HomeController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+/**
+ * 
+ * @author root
+ *
+ */
 @Controller
 public class HomeController {
 	
