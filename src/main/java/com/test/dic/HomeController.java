e10c2c3c4d3dede04553b28a620f6b807ea3aa8e
companion <qpt0707@daum.net>
2017-09-01 09:01:13 +0900

    merage test

--- src/main/java/com/test/dic/HomeController.java
+++ src/main/java/com/test/dic/HomeController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+/**
+ * 
+ * @author Great merage test
+ *
+ */
 @Controller
 public class HomeController {
 	
